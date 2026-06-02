56fd39d567e3403710dcd24d4875faf17391746d
NizmoDev <denispodvin62120@gmail.com>
2026-06-02 15:02:09 +0200

    Tilt

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
 FROM python:3.11
+
 WORKDIR /app
-COPY app.py .
+
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+
+COPY . .
+
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from port 8085 🚀"
+    return "Tilt + Kubernetes + Flask 🚀"
 
 app.run(host="0.0.0.0", port=8085)
